832617e3219220427b3610c67cb4b157b9499f71
Modrum <tristanlaroye@hotmail.com>
2024-03-19 12:34:19 +0100

    fin de la quete

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.yellow("Ileana"));
+console.log(chalk.red("Arnaud"));
+console.log(chalk.blue("Gael Le Mage de la Wild"));
+console.log(chalk.green("Leonard"));
